f2dad2b3fb3bfd1c2c452dcacbc1a7f0e3050c7e
HurDong <gjehdals456@gmail.com>
2025-04-14 01:51:38 +0900

    init : 초기 파일 설정

---
+++ src/main/java/com/hana/investprac/InvestPracApplication.java
@@ -0,0 +1,13 @@
+package com.hana.investprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/investprac/InvestPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.investprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
